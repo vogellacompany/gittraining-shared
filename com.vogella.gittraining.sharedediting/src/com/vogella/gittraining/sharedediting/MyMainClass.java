f0136bf0a98e1047c9e8d059b2b535890058ceea
Lars Vogel <Lars.Vogel@gmail.com>
2013-07-25 23:15:28 +0200

    Initial commit
    Test project for shared changes. Changes are expected to happing mainly
    in Readme.txt and MyMainClass.java to create potentially merge conflicts

---
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/MyMainClass.java
@@ -0,0 +1,12 @@
+package com.vogella.gittraining.sharedediting;
+
+public class MyMainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Add your name to the output");
+		System.out.println("Stefan Lay");
+		System.out.println("Lars Vogel");
+		System.out.println("Markus Kuppe");
+	}
+
+}
