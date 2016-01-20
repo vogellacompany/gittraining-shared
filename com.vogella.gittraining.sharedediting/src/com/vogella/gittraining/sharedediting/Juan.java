04e4bc7a070f2a324651d8892f1f51ebb4b0f0c6
M Damián Fernández <me@manueldamian.com>
2016-01-20 11:53:09 +0100

    Juan Machine

---
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Juan.java
@@ -0,0 +1,9 @@
+package com.vogella.gittraining.sharedediting;
+
+public class Juan {
+
+	public static void main(String[] args) {
+		System.out.println("Hello everyone, I am the Git machine");
+	}
+
+}
