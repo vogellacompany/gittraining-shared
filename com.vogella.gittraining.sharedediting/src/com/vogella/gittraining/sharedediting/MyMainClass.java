729f5027cbc6e30c55e709dad47cf35eb9c05ca6
M Damián Fernández <me@manueldamian.com>
2016-01-20 11:48:40 +0100

    Damián & Juan

--- com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/MyMainClass.java
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/MyMainClass.java
@@ -8,6 +8,7 @@ public class MyMainClass {
 		System.out.println("Lars Vogel");
 		System.out.println("Markus Kuppe");
 		System.out.println("Roberto Sampayo");
+		System.out.println("Damián & Juan");
 	}
 
 }
