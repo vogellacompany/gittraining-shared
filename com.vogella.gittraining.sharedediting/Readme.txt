9d91dedfd209a0a7416170f50420f59f3e0605d7
jorge <jtello@seglan.com>
2016-01-20 11:44:12 +0100

    Changing The Readme file and create a new class

--- com.vogella.gittraining.sharedediting/Readme.txt
+++ com.vogella.gittraining.sharedediting/Readme.txt
@@ -1,1 +1,2 @@
 This is the file which should be changed. 
+Changing the file to the exercise 
---
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/MyEmptyClass.java
@@ -0,0 +1,10 @@
+package com.vogella.gittraining.sharedediting;
+
+public class MyEmptyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
