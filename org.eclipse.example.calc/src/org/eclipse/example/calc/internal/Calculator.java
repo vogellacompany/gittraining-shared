08e52451b5f2fc3a00fa8175873d645e1a5fdb51
VolkmarBurmester <Volkmar.Burmester@generali.de>
2016-04-19 14:45:55 +0200

    First Implementation of Multiply

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -16,6 +16,7 @@ import org.eclipse.example.calc.UnaryOperation;
 import org.eclipse.example.calc.internal.operations.Divide;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
+import org.eclipse.example.calc.internal.operations.Multiply;
 import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
 
@@ -42,6 +43,7 @@ public class Calculator {
 		new Plus();
 		new Square();
 		new Divide();
+		new Multiply();
 	}
 
 	private void calculate(String cmdName) {
