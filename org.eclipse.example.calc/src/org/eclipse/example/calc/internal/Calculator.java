c5f2c645a7e57600ec2660c7e28bd50cb020c19a
Alexander Bruns <alexander.bruns@generali.com>
2016-06-08 14:25:29 +0200

    Add power operation
    A power operation is very common for a calculator. Therefor we would
    also like to have one.

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -17,6 +17,7 @@ import org.eclipse.example.calc.internal.operations.Divide;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
+import org.eclipse.example.calc.internal.operations.Power;
 import org.eclipse.example.calc.internal.operations.Square;
 
 public class Calculator {
@@ -42,6 +43,7 @@ public class Calculator {
 		new Plus();
 		new Square();
 		new Divide();
+		new Power();
 	}
 
 	private void calculate(String cmdName) {
