72268057088f9fb6b87f2b5a45004299252cca20
Christoph Schardt <christoph.schardt@sew-eurodrive.de>
2016-11-03 15:01:35 +0100

    New Divide feature cool

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -13,6 +13,7 @@ import org.eclipse.example.calc.BinaryOperation;
 import org.eclipse.example.calc.Operation;
 import org.eclipse.example.calc.Operations;
 import org.eclipse.example.calc.UnaryOperation;
+import org.eclipse.example.calc.internal.operations.Divide;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
@@ -40,6 +41,7 @@ public class Calculator {
 		new Minus();
 		new Plus();
 		new Square();
+		new Divide();
 	}
 
 	private void calculate(String cmdName) {
