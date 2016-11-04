e2daf56a6b35776cac1720bcab573ab36aa71639
Mario Heer <mario.heer@sew-eurodrive.de>
2016-11-04 11:25:04 +0100

    erster commit

--- com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/MyMainClass.java
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/MyMainClass.java
@@ -14,6 +14,7 @@ public class MyMainClass {
 		System.out.println("Burkhard Mallmes");
 		System.out.println("Matthias Roemmelt");
 		System.out.println("Anonymous");
+		System.out.println("Werauchimmer");
 		System.out.println("Test");
 		
 	}
