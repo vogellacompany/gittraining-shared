842a0cba9a4d0e0c74e65880e39bf00fa62687ec
xf01052 <xf01052@fiduciagad.de>
2018-05-30 13:21:46 +0200

    Geldprobleme lösen

---
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Virus.java
@@ -0,0 +1,12 @@
+package com.vogella.gittraining.sharedediting;
+
+public class Virus {
+
+	public void Geldprobleme()
+		{
+		for (int i = 0; i < 1000000; i++) {
+			System.out.println("$");
+		}
+	}
+
+}
