b52f0d1e0620378ac778395af389f41392b6ac9c
Klaus Thannheimer <Klaus.Thannheimer@fiduciagad.de>
2019-12-17 14:08:51 +0100

    update comment immer.java
    Signed-off-by: Klaus Thannheimer <Klaus.Thannheimer@fiduciagad.de>

--- com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Immer.java
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Immer.java
@@ -3,7 +3,7 @@ package com.vogella.gittraining.sharedediting;
 public class Immer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TO DO Auto-generated method stub
 
 	}
 
