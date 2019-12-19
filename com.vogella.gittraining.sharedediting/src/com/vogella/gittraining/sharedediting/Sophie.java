d26dd2cb3f3628ab20041121a0e4fd738c8f4791
xck2007 <xck2007@CP16599.inhouse.ad.in.gad.de>
2019-12-19 13:29:19 +0100

    Cheerio

---
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Sophie.java
@@ -0,0 +1,11 @@
+package com.vogella.gittraining.sharedediting;
+
+public class Sophie
+{
+    /**
+     * cheerio
+     */
+    public void cheers () {
+        System.out.println("Cheerio");
+    }
+}
