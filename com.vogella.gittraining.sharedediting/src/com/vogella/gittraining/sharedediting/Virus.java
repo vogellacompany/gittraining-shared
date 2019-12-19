65f090a64b4521991813e387233f452a6a2e466b
Maximilian Betz <maximilian.betz@fiduciagad.de>
2019-12-19 13:42:23 +0100

    Die äußere Form eines Virus ist entscheidend => Umformattiert, vielleicht hilftst

--- com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Virus.java
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Virus.java
@@ -1,16 +1,17 @@
 package com.vogella.gittraining.sharedediting;
+
 /**
- * Nein, das ist kein Virus. Sieht nur so aus. Einfach Globuli einwerfen, bitte :)
+ * Nein, das ist kein Virus. Sieht nur so aus. Einfach Globuli einwerfen, bitte
+ * :)
+ * 
  * @author xce3528
  *
  */
 public class Virus {
 
-	public void Geldprobleme()
-		{
+	public void Geldprobleme() {
 		for (int i = 100; i > 0; i--) {
 			System.out.printf("£");
 		}
 	}
-
 }
