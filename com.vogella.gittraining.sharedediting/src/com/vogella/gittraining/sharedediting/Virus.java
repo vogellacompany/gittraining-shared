88fb37233bc7f0a379c93b94542d286189ae8a28
Miriam Wieber <miriam.wieber@fiduciagad.de>
2019-12-19 13:14:20 +0100

    Added comment

--- com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Virus.java
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Virus.java
@@ -1,5 +1,9 @@
 package com.vogella.gittraining.sharedediting;
-
+/**
+ * Nein, das ist kein Virus. Sieht nur so aus. Einfach Globuli einwerfen, bitte :)
+ * @author xce3528
+ *
+ */
 public class Virus {
 
 	public void Geldprobleme()
