9f2a18accad253e340c3cbf2f48557abdb9f9619
Thomas Körner <Thomas.Koerner@fiduciagad.de>
2020-01-15 13:52:39 +0100

    Mein Beitrag zur Schulung.

---
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/TCorn.java
@@ -0,0 +1,14 @@
+package com.vogella.gittraining.sharedediting;
+
+public class TCorn
+{
+
+    String m_greetings;
+
+    public TCorn(String greetings)
+    {
+        m_greetings = "Hello World!";
+    }
+
+
+}
