282ef0ef70d65c9b4f8f648306f8fb293c299859
Sarah Leiting <sarah.leiting@fiduciagad.de>
2020-01-31 13:50:43 +0100

    Kommentare zur erläuterung der Klasse Always

--- com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Allways.java
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/Allways.java
@@ -3,4 +3,13 @@ package com.vogella.gittraining.sharedediting;
 public class Allways
 {
 
+    // diese Klasse ist umbenannt worden, das ist mir aber egal
+    // möchte jemand aus diesem Kurs einen Konflikt erzeugen?
+
+    // hier ist Platz für ihr Kommentar
+
+    /*
+     * 
+     */
+
 }
