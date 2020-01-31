72ba6ccc6092dcd1a2109b0ae6c0843280f3fd33
Kai Krieger <Kai.Krieger@fiduciagad.de>
2020-01-31 13:53:31 +0100

    Mehr Kommentar an SuperiorClass

--- com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/SuperiorClass.java
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/SuperiorClass.java
@@ -3,7 +3,10 @@ package com.vogella.gittraining.sharedediting;
 /**
  * Überlegener Kommentar
  * <p>
- * Ein langer Kommentar ist nicht notwendig
+ * Ein langer Kommentar ist nicht notwendig.
+ * Aber weil ich Platz habe, schreibe ich trotzdem noch irgendwas hier hin.
+ * Wer soweit gekommen ist, sollte wirklich von dieser Klasse erben. Vielleicht gibts irgenwann 
+ * auch noch eine Implementierung und verändert dann das Verhalten aller Subklassen. MUUHAHAHA
  */
 public class SuperiorClass
 {
