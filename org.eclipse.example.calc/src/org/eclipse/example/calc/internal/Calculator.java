f6175e85e6aa95aa1a9c09e6e53cfc79980abc22
Malte Weber <Malte.Weber@atruvia.de>
2021-09-14 14:33:42 +0200

    Function Icrement

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -17,6 +17,7 @@ import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
+import org.eclipse.example.calc.internal.operations.Increment;
 
 public class Calculator {
 
@@ -42,6 +43,7 @@ public class Calculator {
 		new Minus();
 		new Plus();
 		new Square();
+		new Increment();
 		//What??
 //		new Multiply();
 //        new Divide();
