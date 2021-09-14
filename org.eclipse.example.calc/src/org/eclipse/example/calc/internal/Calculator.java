60d2104b8c1b9d56890abe739bd010d6a28137ab
Ute Scharff <ute.scharff@atruvia.de>
2021-09-14 14:35:40 +0200

    Change Titel

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -28,7 +28,7 @@ public class Calculator {
 
 	private float value;
 
-	public static String NAME = "Simple Calculator";
+	public static String NAME = "MWP Calculator";
 
 	public Calculator(TextProvider textProvider) {
 		this.textProvider = textProvider;
