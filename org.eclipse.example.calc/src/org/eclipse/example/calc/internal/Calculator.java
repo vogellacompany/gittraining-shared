aa3e71e7d13a821fc5f26955ec24c18a66bc416a
Marius Lietmeyer <Marius.Lietmeyer@atruvia.de>
2022-01-27 14:03:56 +0100

    unnötige Kommentare gelöscht

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -40,8 +40,6 @@ public class Calculator {
 	}
 
 	private void setupDefaultOperations() {
-		// Ein weiterer Kommentar :) von Fidunico
-	    // Nur ein Kommentart
 		new Equals();
 		new Minus();
 		new Plus();
