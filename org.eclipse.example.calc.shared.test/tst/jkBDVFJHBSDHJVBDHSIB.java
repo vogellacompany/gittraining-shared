396cd8a099ba0d0ce64c465a1adeaa8d2d0eaf2e
Gnuru <gerrit-jan.nuys@atruvia.de>
2022-05-19 14:21:03 +0200

    Übung

---
+++ org.eclipse.example.calc.shared.test/tst/jkBDVFJHBSDHJVBDHSIB.java
@@ -0,0 +1,6 @@
+
+public class jkBDVFJHBSDHJVBDHSIB {
+	
+	/*jfdbjsbjkgfjklgjlkglkblksfghblkgflkbh*/
+
+}
--- org.eclipse.example.calc.shared.test/tst/org/eclipse/example/calc/OperationsTest.java
+++ org.eclipse.example.calc.shared.test/tst/org/eclipse/example/calc/OperationsTest.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
- * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
+ * Copyright (C) 2010, Matthias Sohn <matthiFEFas.sohn@sap.com>
  *
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the term<SDdfvfdvfvds of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
