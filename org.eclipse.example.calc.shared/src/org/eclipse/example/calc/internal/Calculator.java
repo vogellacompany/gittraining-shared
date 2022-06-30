bfdf2c2d5d68203f6a223c234335143b2ebe78d5
Gebhard Debatin <gebhard.debatin@atruvia.de>
2022-06-30 14:23:38 +0200

    neue Operation GerneEtwasMehr

--- org.eclipse.example.calc.shared/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc.shared/src/org/eclipse/example/calc/internal/Calculator.java
@@ -14,6 +14,7 @@ import org.eclipse.example.calc.Operation;
 import org.eclipse.example.calc.Operations;
 import org.eclipse.example.calc.UnaryOperation;
 import org.eclipse.example.calc.internal.operations.Equals;
+import org.eclipse.example.calc.internal.operations.GerneEtwasMehr;
 import org.eclipse.example.calc.internal.operations.Minus;
 import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
@@ -41,6 +42,7 @@ public class Calculator {
 		new Minus();
 		new Plus();
 		new Square();
+		new GerneEtwasMehr();
 	}
 
 	private void calculate(String cmdName) {
