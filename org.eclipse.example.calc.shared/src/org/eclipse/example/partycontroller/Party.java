84926afb0870be4fbf8e3414266b040ae09a2262
Maik Drobil <ulfgarhammerschlag@googlemail.com>
2022-10-27 14:05:59 +0200

    Partymodul initialisiert Wichtige Addition zur Party Klasse

---
+++ org.eclipse.example.calc.shared/src/org/eclipse/example/partycontroller/Party.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (C) 2010, Matthias Sohn <matthias.sohn@sap.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eclipse.example.partycontroller;
+
+import java.lang.reflect.Array;
+
+public class Party {
+	final boolean nocharts = true;
+	public void start(Array playlist, int location_id) {
+		
+	}
+
+	
+	public void stop() {
+		
+	}
+
+}
